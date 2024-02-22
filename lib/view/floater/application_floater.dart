cfad3cf9e7f67faac2e3d5043dd5aa801107f8c5
woodyvillage <70624866+woodyvillage@users.noreply.github.com>
2024-02-23 01:58:36 +0900

    code refine

--- lib/view/floater/application_floater.dart
+++ lib/view/floater/application_floater.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 import 'package:stash_book/const/routing_const.dart';
 import 'package:stash_book/service/floater_action_service.dart';
 import 'package:stash_book/service/navigator_position_service.dart';
