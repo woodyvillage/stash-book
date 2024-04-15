0cbe9a7c5b7024315ecef6af33cbd9c0ce029874
woodyvillage <70624866+woodyvillage@users.noreply.github.com>
2024-04-16 05:04:16 +0900

    table scope criteria DTO to DAO

--- lib/model/data/dao/abstract/base_dao.dart
+++ lib/model/data/dao/abstract/base_dao.dart
@@ -1,18 +1,18 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:stash_book/const/database_const.dart';
 import 'package:stash_book/database/application_database.dart';
-import 'package:stash_book/model/data/dto/account_dto.dart';
-import 'package:stash_book/model/data/dto/possession_dto.dart';
-import 'package:stash_book/model/data/dto/setting_dto.dart';
+import 'package:stash_book/model/data/dao/account_dao.dart';
+import 'package:stash_book/model/data/dao/possession_dao.dart';
+import 'package:stash_book/model/data/dao/setting_dao.dart';
 
 abstract class BaseDao {
-  // DTOの種類から対象のテーブルを指定する
+  // DAOの種類から対象のテーブルを指定する
   String scope(dynamic dto) {
-    if (dto is PossessionDto) {
+    if (dto is PossessionDao) {
       return DatabaseConst.tablePossession;
-    } else if (dto is SettingDto) {
+    } else if (dto is SettingDao) {
       return DatabaseConst.tableSetting;
-    } else if (dto is AccountDto) {
+    } else if (dto is AccountDao) {
       return DatabaseConst.tableAccount;
     } else {
       return "";
