61c4d10683ae2cb877e1de83a0b769f382d2fb5a
woodyvillage <70624866+woodyvillage@users.noreply.github.com>
2024-08-11 13:15:43 +0900

    refine implements

--- lib/model/form/dto/abstract/base_form_dto.dart
+++ lib/model/form/dto/abstract/base_form_dto.dart
@@ -1,6 +1,6 @@
-import 'package:stash_book/model/form/dto/favorite_dto_impl.dart';
+import 'package:stash_book/model/form/dto/implement/base_form_impl.dart';
 
-abstract class BaseFormDto implements FavoriteDtoImpl {
+abstract class BaseFormDto implements BaseFormImpl {
   List<List<Object>>? items;
 
   BaseFormDto({
--- lib/model/form/dto/favorite_dto_impl.dart
+++
@@ -1,3 +0,0 @@
-abstract class FavoriteDtoImpl {
-  Object get dto;
-}
